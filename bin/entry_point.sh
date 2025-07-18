579f70f9575ee91fd7f556e4dde64fbcba6fe46a
ChengWang2018 <18771014894@163.com>
2025-07-18 16:06:31 +0100

    update config

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -2,7 +2,10 @@
 
 CONFIG_FILE=_config.yml 
 
-/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+# Ensure git sources are properly checked out
+bundle install
+
+/bin/bash -c "exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
 while true; do
 
@@ -15,7 +18,10 @@ while true; do
     jekyll_pid=$(pgrep -f jekyll)
     kill -KILL $jekyll_pid
 
-    /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+    # Ensure git sources are properly checked out
+    bundle install
+    
+    /bin/bash -c "exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
   fi
 
